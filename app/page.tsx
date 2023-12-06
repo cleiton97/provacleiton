74616066734f1d578c1b400896d4b11d4a5f8a5e
cleiton97 <146895519+cleiton97@users.noreply.github.com>
2023-12-06 01:09:06 +0000

    corrigindo @vercel/postgres

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+
+import NewCourse from "./course/new/page";
+
+export const revalidate = 0
+export default function Home() {
+  return (
+    
+      <div className="max-w-[1440px] m-auto ">
+      
+        <NewCourse/>
+
+
+      </div>
+    
+
+
+  )
+}
