7ccb95c564173a52e34813b31549ae77a2bdb095
cleiton97 <146895519+cleiton97@users.noreply.github.com>
2023-12-06 00:43:07 +0000

    commit

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,7 +2,7 @@ export default function Admin(){
     return(
         <div>
         <h1 className="text-center text-xl"> Area  da Estudante</h1>
-        <a href="/admin/course/">CADASTROS-</a>
+        <a href="/admin/course/">CADASTRO-</a>
         <a href="/admin/course/new">    --LISTAGEM</a>
         </div>
     )
